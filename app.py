c94e742b71fc78ec29fd66da15164a532d0e82c2
user <user@users-MacBook-Air.local>
2018-11-18 17:29:35 -0600

    homework update

--- app.py
+++ app.py
@@ -4,21 +4,25 @@ import scrape_mars
 
 app = Flask(__name__)
 
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-mongo = PyMongo(app)
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+#mongo = PyMongo(app)
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars")
 
 @app.route("/")
 def index():
-    mars = mongo.db.mars.find_one()
+    mars = mongo.db.collection.find_one()
     return render_template("index.html",mars=mars)
 
 
 @app.route("/scrape")
 def scraper():
-    mars = mongo.db.mars
     mars_dict = scrape_mars.scrape()
-    mars.update({}, mars_dict, upsert=True)
-    return redirect("/", code=302)
+    mongo.db.collection.update({}, mars_dict, upsert=True)
+    return redirect("/")
+    # mars = mongo.db.mars
+    # mars_dict = scrape_mars.scrape()
+    # mars.update({}, mars_dict, upsert=True)
+    # return redirect("/", code=302)
 
 
 if __name__ == "__main__":
--- scrape_mars.py
+++ scrape_mars.py
@@ -2,7 +2,13 @@ from bs4 import BeautifulSoup
 import requests
 from splinter import Browser
 import pandas as pd
+import sys
+import time
 
+sys.setrecursionlimit(10000)
+def init_browser():
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
 
 def scrape():
     url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'    
@@ -13,8 +19,8 @@ def scrape():
     news_title = news.text.strip()
     news_paragraph = soup.find_all('div', class_="rollover_description_inner")[0]
     news_p = news_paragraph.text.strip()
-    mars_dict['news title']=news_title
-    mars_dict['news paragraph']=news_p
+    mars_dict['news_title']=news_title
+    mars_dict['news_paragraph']=news_p
     executable_path = {'executable_path': 'chromedriver'}
     browser = Browser('chrome', **executable_path, headless=False)
     image_link = 'https://www.jpl.nasa.gov'
@@ -30,19 +36,19 @@ def scrape():
         img = result.find('a')
     img_url = img['href']
     featured_image_url = image_link + img_url
-    mars_dict['featured image url'] = featured_image_url
+    mars_dict['featured_image_url'] = featured_image_url
     url3='https://twitter.com/marswxreport?lang=en'
     response = requests.get(url3)
     soup = BeautifulSoup(response.text, 'html.parser')
     mars_tweet = soup.find_all('p', class_="TweetTextSize")[0]
     mars_weather = mars_tweet.text.strip()
-    mars_dict['mars weather'] = mars_weather
+    mars_dict['mars_weather'] = mars_weather
     url4 = 'https://space-facts.com/mars/'
     tables = pd.read_html(url4)
     mars_df = tables[0]
     mars_df.rename(columns={1:'Values', 0:'Facts'}).set_index('Facts')
     mars_df_html = mars_df.to_html(index=False).replace('\n', "")
-    mars_dict['mars html'] = mars_dict
+    mars_dict['mars_html'] = mars_dict
     executable_path = {'executable_path': 'chromedriver'}
     browser = Browser('chrome', **executable_path, headless=False)
     url5='https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
@@ -69,6 +75,11 @@ def scrape():
         astro_dict['image_url'] = img_url
         astro_dict['title'] = title_url
         mars_list.append(astro_dict)
-    mars_dict['mars list'] = mars_list
-
+        time.sleep(3)
+    mars_dict['mars_list'] = mars_list
+    # mars_dict['cerberus'] = mars_list[0]
+    # mars_dict['schiaparelli'] = mars_list[1]
+    # mars_dict['syrtis_major'] = mars_list[2]
+    # mars_dict['valles_marineris'] = mars_list[3]
+    browser.quit()
     return mars_dict
